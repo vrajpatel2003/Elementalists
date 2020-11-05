89fcb56f2a6e933f3bbbd4db1bac3ce0433f802f
neilp <neilplays64@gmail.com>
2020-11-05 18:37:16 -0500

    yooooooooo

--- src/Elementalists.java
+++ src/Elementalists.java
@@ -25,6 +25,7 @@ public class Elementalists {
         System.out.println("gorilla");
         System.out.println("gorilla");
         System.out.println("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT7WblTnXZl1tiLQv3H2Ls_EfbSEhpWboZ3Lw&usqp=CAU");
+        System.out.println("yo why is this so dumb");
     }
 
     public static void HelloWorld3() {
